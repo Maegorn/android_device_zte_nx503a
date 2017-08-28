03f086696d32ea2e9bdd3b0743c833c4e7d11471
Maegorn <mattia.pellegrino@live.it>
2017-08-28 17:09:08 +0200

    nx503a: s@vendor/cm@vendor/lineage@g

--- lineage.mk
+++ lineage.mk
@@ -28,7 +28,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/zte/nx503a/nx503a.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := nx503a
